8c78aa55b3ad22fe914641170f3368da76c699b1
Sebastian Landwehr <info@sebastianlandwehr.com>
2024-03-15 18:21:50 +0100

    fix: fix config

--- src/index.js
+++ src/index.js
@@ -8,41 +8,44 @@ import ecosystem from './ecosystem/index.js'
 
 const packageConfig = loadPkg.sync()
 
-export default config => ({
-  ...baseConfigNode(config),
-  allowedMatches: [...baseConfigNode.allowedMatches, 'ecosystem.json'],
-  editorIgnore: [...baseConfigNode.editorIgnore, 'ecosystem.json'],
-  isLockFileFixCommitType: true,
-  npmPublish: false,
-  packageConfig: {
-    main: 'dist/index.js',
-  },
-  prepare: () =>
-    fs.outputFile('ecosystem.json', JSON.stringify(ecosystem, undefined, 2)),
-  ...(!packageConfig.private && {
-    deployPlugins: [
-      [
-        packageName`@semantic-release/exec`,
-        {
-          publishCmd: `${packageName`pm2`} deploy production --force`,
-        },
-      ],
-    ],
-    preDeploySteps: [
-      {
-        uses: 'webfactory/ssh-agent@v0.5.1',
-        with: {
-          'ssh-private-key': '${{ secrets.SSH_PRIVATE_KEY }}',
-        },
-      },
-      { run: 'ssh-keyscan sebastianlandwehr.com >> ~/.ssh/known_hosts' },
-    ],
-  }),
-  commands: {
-    ...baseConfigNode.commands,
-    setupDeploy: {
-      handler: () =>
-        execa.command('pm2 deploy production setup', { stdio: 'inherit' }),
+export default config => {
+  const nodeConfig = baseConfigNode(config)
+  return {
+    ...nodeConfig,
+    allowedMatches: [...nodeConfig.allowedMatches, 'ecosystem.json'],
+    editorIgnore: [...nodeConfig.editorIgnore, 'ecosystem.json'],
+    isLockFileFixCommitType: true,
+    npmPublish: false,
+    packageConfig: {
+      main: 'dist/index.js',
     },
-  },
-})
+    prepare: () =>
+      fs.outputFile('ecosystem.json', JSON.stringify(ecosystem, undefined, 2)),
+    ...(!packageConfig.private && {
+      deployPlugins: [
+        [
+          packageName`@semantic-release/exec`,
+          {
+            publishCmd: `${packageName`pm2`} deploy production --force`,
+          },
+        ],
+      ],
+      preDeploySteps: [
+        {
+          uses: 'webfactory/ssh-agent@v0.5.1',
+          with: {
+            'ssh-private-key': '${{ secrets.SSH_PRIVATE_KEY }}',
+          },
+        },
+        { run: 'ssh-keyscan sebastianlandwehr.com >> ~/.ssh/known_hosts' },
+      ],
+    }),
+    commands: {
+      ...nodeConfig.commands,
+      setupDeploy: {
+        handler: () =>
+          execa.command('pm2 deploy production setup', { stdio: 'inherit' }),
+      },
+    }
+  }
+}
